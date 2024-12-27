79c704a5c9a1766895f4b693ea31617a64b5335a
13-arch <Iron_Mike@tutamail.com>
2024-12-27 13:25:39 +0300

    27.12.24

--- main.py
+++ main.py
@@ -4,22 +4,28 @@
 
 
 # import all nessesary libs and modules
-import Euclid as E
+import complex_module.Euclid as E
 import Pascal as P
 import notations as n
 
 
 # main function
 def main():
-    user_chiose = (input("Choose E if you want to use Euclid: "))
+    user_chiose = (input("Choose E,P,N if you want to use Euclid,Paskal,Notations: "))
     if user_chiose.lower() =='e':
         ecl= (input("Введите два числа через пробел: "))
         inp_is =ecl.split()
         inp_1 = int(inp_is[0]) # преобразование в int
         inp_2 = int(inp_is[1])
         print('nod chisel raven:', E.gcd(inp_1,inp_2))
-    if user_chiose.lower()=='p':
-        pass
+    elif user_chiose.lower() == 'n':
+        n.main()
+        main()
+    elif user_chiose.lower() == 'p':
+        P.main()
+        main()
+    else:
+        return 1
     
 
 if __name__=='__main__':
